94e0d7e67d7cb6cacbdf7c7c5c50c52d6fcdda6c
tarellium <tarellium1@gmail.com>
2017-04-28 00:22:51 -0400

    danny

---
+++ selenium/src/selenium/Dany.java
@@ -0,0 +1,29 @@
+package selenium;
+
+public class Dany {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+				// TODO Auto-generated method stub
+		     int x=4;
+				do
+				{System.out.println(x);
+				x=x+1;
+				} 
+				while(x<20);
+				
+				int count = 1;
+				 do {
+				 System.out.println("Count is: " + count);
+				 count++;
+				 } 
+				 while (count < 11);
+					
+					
+				}
+
+	}
+
+
